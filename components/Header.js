30618ccbef01fd0dc5dad3ce5937da5123d3c529
Bwise <oguntoyebenjamin2@gmail.com>
2021-10-13 13:00:41 +0300

    few changes to right header

--- components/Header.js
+++ components/Header.js
@@ -44,10 +44,21 @@ function Header() {
                 <div className="flex items-center justify-end space-x-4">
                     <HomeIcon className="navBtn" />
                     <MenuIcon className="h-6 md:hidden cursor-pointer" />
-                    <PaperAirplaneIcon className="navBtn" />
+
+                    <div className="relative navBtn">
+                        <PaperAirplaneIcon className="navBtn rotate-45" />
+                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 
+                        rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+                    </div>
                     <PlusCircleIcon className="navBtn" />
                     <UserGroupIcon className="navBtn" />
                     <HeartIcon className="navBtn" />
+
+                    <img
+                        src="https://drive.google.com/uc?export=view&id=1r4gkKYpgtHrjPg1knh8rgIKJqLz0xyZ1"
+                        alt="profile picture"
+                        className="h-10 rounded-full cursor-pointer"
+                    />
                 </div>
             </div>
         </div>
