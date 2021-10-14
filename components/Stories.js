2da2633767a872df8e4829de38b424b370003078
Bwise <oguntoyebenjamin2@gmail.com>
2021-10-14 10:41:36 +0300

    user profile styling

--- components/Stories.js
+++ components/Stories.js
@@ -15,7 +15,8 @@ function Stories() {
         setSuggestions(suggestions);
     }, [])
     return (
-        <div>
+        <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 
+        border rounded-sm overflow-x-scroll">
             {suggestions.map(profile => (
                 <Story 
                     key={profile.id} 
