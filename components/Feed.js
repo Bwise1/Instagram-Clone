cb49109d60f0527873be06982686a6e295b6b822
Bwise <oguntoyebenjamin2@gmail.com>
2021-10-14 08:53:52 +0300

    feed part structuring

---
+++ components/Feed.js
@@ -0,0 +1,24 @@
+import Stories from "./Stories"
+
+function Feed() {
+    return (
+        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl 
+        xl:grid-cols-3 xl:max-w-6xl mx-auto">
+            {/* Left part */}
+            <section className="col-span-2">
+                {/* stories */}
+                <Stories />
+
+                {/* posts */}
+            </section>
+            
+            {/* Right part */}
+            <section>
+                {/* Mini Profile */}
+                {/* Left part */}
+            </section>
+        </main>
+    )
+}
+
+export default Feed
